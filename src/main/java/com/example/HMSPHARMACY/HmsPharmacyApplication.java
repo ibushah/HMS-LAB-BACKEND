211acdd112f20588a7ef0027ea984b591bd2fa7a
ibushah <username.ibu@gmail.com>
2019-08-31 20:07:11 +0500

    first commit

---
+++ src/main/java/com/example/HMSPHARMACY/HmsPharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.example.HMSPHARMACY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsPharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsPharmacyApplication.class, args);
+	}
+
+}
