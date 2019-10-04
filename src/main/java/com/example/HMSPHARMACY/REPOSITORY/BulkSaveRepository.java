c9dd01da67cab385382a6dd02fd23cd929aa208f
m-asadd1997 <qureshiasad1000@gmail.com>
2019-10-05 02:16:27 +0500

    sales filter work

--- src/main/java/com/example/HMSPHARMACY/REPOSITORY/BulkSaveRepository.java
+++ src/main/java/com/example/HMSPHARMACY/REPOSITORY/BulkSaveRepository.java
@@ -2,9 +2,16 @@ package com.example.HMSPHARMACY.REPOSITORY;
 
 import com.example.HMSPHARMACY.MODEL.BulkSave;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BulkSaveRepository extends JpaRepository<BulkSave,Long>{
 
+
+    @Query(value = "select * from bulk_save where created_at between :from and :till order by created_at",nativeQuery = true)
+    public List<BulkSave> getBulkSaveByCreatedAt(@Param("from") String from,@Param("till") String till);
 }
